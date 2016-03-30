4a3b7205fb819c0929c6d28546be0f8a0d32c99f
Steven WATREMEZ <steven.watremez@niji.fr>
2016-03-30 09:41:25 +0200

    init repo with Xcode project in Swift

---
+++ youcodeio/youcodeio/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  youcodeio
+//
+//  Created by Steven WATREMEZ on 30/03/16.
+//  Copyright © 2016 Steven WATREMEZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
